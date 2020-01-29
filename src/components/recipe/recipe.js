21534cc28ac86c312ce4d669a7e11de976a88557
Bikelund <nicklas_barklund@hotmail.com>
2020-01-29 14:35:27 +0100

    Swiper pagination and navigation added to recipe

--- src/components/recipe/recipe.js
+++ src/components/recipe/recipe.js
@@ -6,6 +6,7 @@ import 'swiper/swiper.scss';
 function Recipe() {
 
     const params = {
+        initialSlide: 1, /* Starting slide index */
         speed: 600,
         parallax: true,
         parallaxEl: {
@@ -20,26 +21,45 @@ function Recipe() {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
         }
-      }
+    }
 
     return (
         <Swiper {...params}>
+            {/* Slide Ingredients */}
             <div className="recipe__ingredients">
                 <h3>Ingredients</h3>
+                <ul>
+                  {/* data-swiper-parallax creates a fade in delay */}
+                  <li data-swiper-parallax="100">Ingredient One</li>
+                  <li data-swiper-parallax="0">Ingredient Two</li>
+                  <li data-swiper-parallax="-100">Ingredient Three</li>
+                  <li data-swiper-parallax="-200">Ingredient Four</li>
+                  <li data-swiper-parallax="-300">Ingredient Five</li>
+                </ul>
+
+                <div className="button__container button__container--left">
+                  <p>Höger</p>
+                </div>
 
             </div>
-            <div className="recipe__title">
-                <h2 data-swiper-parallax="-300">"Title of the recipe"</h2>
-                <p data-swiper-parallax="-200">"Title of the recipe"</p>
-                <p data-swiper-parallax="-100">"Title of the recipe"</p>
 
-                {/* <div className="button__container">
-                    <button className="button__container--button-left">&#60;&#60; Ingredients</button>
-                    <button className="button__container--button-right">How to cook &#62;&#62;</button>
-                </div> */}
+            {/* Slide Title */}
+            <div className="recipe__title" data-initial-slide="1"> {/* Makes this the initial slide */}
+                <h2>"Title of the recipe"</h2>
+
+                <div className="button__container">
+                  <p>Vänster</p>
+                  <p>Höger</p>
+                </div>
             </div>
+
+            {/* Slide How to cook */}
             <div className="recipe__how-to-cook">
                 <h3>How to cook</h3>
+
+                <div className="button__container button__container--right">
+                  <p>Vänster</p>
+                </div>
 
             </div>
         </Swiper>
