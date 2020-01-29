ce9760900afa80222d6eba79777b4a740dd4490b
Toshiko <kunotoshiko17@gmail.com>
2020-01-29 17:13:03 +0100

    working with my recipes component

--- src/components/myRecipes/myRecipes.js
+++ src/components/myRecipes/myRecipes.js
@@ -1,35 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import firebase from 'firebase';
 
 function MyRecipes() {
+    const recipes = [];
 
-//     await firebase.firestore()
-//   .collection('/recipie')
-//   .doc(user.uid)
-//   .get()
-//   .then(function(doc) {
-//     if (doc.exists) {
-//       // do something
-//       // doc.data() でデータを取得
-//     } else {
-//       console.log("No user");
-//     }
-//   })
-//   .catch(function(error) {
-//     console.log("Error : ", error);
-//   })
-firebase.auth().onAuthStateChanged(user => {
-
-    firebase.firestore().collection('users').doc('recipes').get().then((snapshot) => {
-        snapshot.docs.forEach(e => {
-             console.log(e.data().category)
+    useEffect(() => {
+         firebase.auth().onAuthStateChanged(user => {
+            firebase.firestore().collection('users').doc(user.uid).collection('recipes').get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        if (doc.exists) {
+                            recipes.push(doc.data())
+                        } else {
+                            console.log("No such document!");
+                        }
+                    });
+                }).catch(function (error) {
+                    console.log("Error getting document:", error);
+                });
         })
-    })
-})
+    },[])
+    
     return (
         <>
             <h1>My Recipes</h1>
-
+            {console.log(recipes)}
         </>
     )
 }
