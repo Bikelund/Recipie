82502dc15d08ed242fb4cd05cee484032956d17b
Toshiko <kunotoshiko17@gmail.com>
2020-02-02 19:51:42 +0100

    Added styling for createRecipe and back button on createRecipe component

--- src/components/createRecipe/createRecipe.js
+++ src/components/createRecipe/createRecipe.js
@@ -3,8 +3,10 @@ import firebase from '../firebase/firebase';
 import 'firebase/firestore';
 import { firestore } from 'firebase';
 import { v4 as uuid } from 'uuid';
+import MyRecipes from '../myRecipes/myRecipes';
 
 function CreateRecipe() {
+    const [backToPage, setBackToPage] = useState(false)
     const [title, setTitle] = useState('')
     const [servings, setServings] = useState('')
     const [category, setCategory] = useState('')
@@ -13,12 +15,12 @@ function CreateRecipe() {
     function handleSubmit(event) {
         event.preventDefault()
         firebase.auth().onAuthStateChanged(user => {  //Check if user is logged in
-        const firestoreRef = firebase.firestore().collection('users').doc(user.uid).collection('recipes'); //Create a firestore and child reference 
-        const filename = image.name; //Get image name from image state
-        const ext = filename.slice(filename.lastIndexOf("."));
-        const storageRef = firebase.storage().ref('recipes').child(uuid() + "." + ext); //Create a storage and recipes reference. uuid() is generator to make unique key in every image
+            const firestoreRef = firebase.firestore().collection('users').doc(user.uid).collection('recipes'); //Create a firestore and child reference 
+            const filename = image.name; //Get image name from image state
+            const ext = filename.slice(filename.lastIndexOf("."));
+            const storageRef = firebase.storage().ref('recipes').child(uuid() + "." + ext); //Create a storage and recipes reference. uuid() is generator to make unique key in every image
 
-        storageRef.put(image) //File uploaded
+            storageRef.put(image) //File uploaded
                 .then(() => {
                     return storageRef.getDownloadURL(); //To get image url
                 })
@@ -38,41 +40,64 @@ function CreateRecipe() {
                     console.error("error: " + error);
                 });
 
-            })
+        })
     }
 
 
     return (
-        <>
-
-            <div>Create Recipe</div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    name="title"
-                    value={title}
-                    type="text"
-                    placeholder="title"
-                    onChange={e => setTitle(e.target.value)}
-                /><br />
-                <input
-                    name="category"
-                    value={category}
-                    type="category"
-                    placeholder="category"
-                    onChange={e => setCategory(e.target.value)}
-                /><br />
-                <input
-                    name="servings"
-                    value={servings}
-                    type="servings"
-                    placeholder="servings"
-                    onChange={e => setServings(e.target.value)}
-                /><br />
-                    <input type="file" name="pic" onChange={e => setImage(e.target.files[0])}/>
-                <button type="submit">
-                    ADD RECEPT
-                </button>
-            </form>
+        <>{backToPage ?
+            <MyRecipes /> :
+            <>
+                <div onClick={() => setBackToPage(true)} className="arrow"></div>
+                <div className="create__recipe__title">Create Recipe</div>
+                <form className="create__recipe__form" onSubmit={handleSubmit}>
+                    <div className="create__recipe__form__input">
+                        <label for="title">
+                            <div className="create__recipe__form__input__title">title</div>
+                            <input
+                                name="title"
+                                id="title"
+                                className="create__recipe__form__input__text"
+                                value={title}
+                                type="text"
+                                placeholder="title"
+                                onChange={e => setTitle(e.target.value)}
+                            /><br />
+                        </label>
+                        <label for="category">
+                        <div className="create__recipe__form__input__title">category</div>
+                            <input
+                                name="category"
+                                id="category"
+                                className="create__recipe__form__input__text"
+                                value={category}
+                                type="category"
+                                placeholder="category"
+                                onChange={e => setCategory(e.target.value)}
+                            /><br />
+                        </label>
+                        <label for="servings">
+                        <div className="create__recipe__form__input__title">servings</div>
+                            <input
+                                name="servings"
+                                className="create__recipe__form__input__text"
+                                value={servings}
+                                type="servings"
+                                placeholder="servings"
+                                onChange={e => setServings(e.target.value)}
+                            /><br />
+                        </label>
+                        <label className="create__recipe__form__input__image__title" for="file_upload">Image
+                        <div className="create__recipe__form__input__image__icon"></div>
+                            <input id="file_upload" className="create__recipe__form__input__image" type="file" name="pic" onChange={e => setImage(e.target.files[0])} />
+                        </label>
+                    </div>
+                    <button className="create__recipe__form__button" type="submit">
+                        ADD RECEPT
+                    </button>
+                </form>
+            </>
+        }
         </>
     )
 }
