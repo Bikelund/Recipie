50234f8ea44f32321c00ccb25bfd0a6e3b204471
Bikelund <nicklas_barklund@hotmail.com>
2020-02-06 11:46:11 +0100

    Recipe text has clear outline and focus

--- src/components/recipe/recipe.js
+++ src/components/recipe/recipe.js
@@ -32,14 +32,20 @@ function Recipe( props ) {
       <Swiper {...params}>
         {/* Slide Ingredients */}
         <div className="recipe__ingredients">
-          <h2>Ingredients</h2>
-          <ul className="recipe__ingredients__ul">
-            { 
-              !recipe.ingredients === true ? <li className="recipe__ingredients__ul__li">You have all you need</li>
-              :recipe.ingredients.map((item, key) => <li data-swiper-parallax={"-" + key + "0"} key={key} className="recipe__ingredients__ul__li">{item}</li>)
-              // data-swiper-parallax creates a fade in delay 
-            }
-          </ul>
+          <div className="container">
+            <div className="container__blur"></div>
+            <h2 className="container__h2">Ingredients</h2>
+          </div>
+          <div className="container">
+            <div className="container__blur__list"></div>
+            <ul className="recipe__ingredients__ul">
+              { 
+                !recipe.ingredients === true ? <li className="recipe__ingredients__ul__li">You have all you need</li>
+                :recipe.ingredients.map((item, key) => <li data-swiper-parallax={"-" + key + "0"} key={key} className="recipe__ingredients__ul__li">{item}</li>)
+                // data-swiper-parallax creates a fade in delay 
+              }
+            </ul>
+          </div>
 
           <div className="button__container button__container--left fontAwesome">
             <p>Back &#xf105;</p>
@@ -49,7 +55,10 @@ function Recipe( props ) {
 
         {/* Slide Title */}
         <div className="recipe__title" data-initial-slide="1"> {/* Makes this the initial slide */}
-          <h1>{recipe.title}</h1>
+          <div className="container">
+              <div className="container__blur"></div>
+            <h1 className="container__h1">{recipe.title}</h1>
+          </div>
           <div onClick={() => history.goBack()} className="arrow fontAwesome"></div>
           <div className="bgImg" style={style}></div>
           <button className="recipe__edit" onClick={() => history.push({
@@ -64,14 +73,20 @@ function Recipe( props ) {
 
         {/* Slide How to cook */}
         <div className="recipe__how-to-cook">
-          <h2>How to cook</h2>
-          <ol className="recipe__how-to-cook__ol">
-            { 
-              !recipe.directions === true ? <li className="recipe__how-to-cook__ol__li">Just eat it</li>
-              :recipe.directions.map((item, key) => <li data-swiper-parallax={"-" + key + "0"} key={key} className="recipe__how-to-cook__ol__li">{item}</li>)
-              // data-swiper-parallax creates a fade in delay      
-            }
-          </ol>
+          <div className="container">
+              <div className="container__blur"></div>
+            <h2 className="container__h2">How to cook</h2>
+          </div>
+          <div className="container">
+            <div className="container__blur__list"></div>
+            <ol className="recipe__how-to-cook__ol">
+              { 
+                !recipe.directions === true ? <li className="recipe__how-to-cook__ol__li">Just eat it</li>
+                :recipe.directions.map((item, key) => <li data-swiper-parallax={"-" + key + "0"} key={key} className="recipe__how-to-cook__ol__li">{item}</li>)
+                // data-swiper-parallax creates a fade in delay      
+              }
+            </ol>
+          </div>
 
           <div className="button__container button__container--right fontAwesome">
             <p>&#xf104; Back</p>
